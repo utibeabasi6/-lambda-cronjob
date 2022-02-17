e248e4bc321a8bf09b07ae9bcc3c882688a8f955
utibeabasi6 <utibeabasiumanah6@gmail.com>
2022-02-17 08:01:48 +0100

    added remote backend

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = var.state_bucket
+    key    = "terraform.tfstate"
+    region = var.aws_region
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
     type = string
     description = "The region in which to create the lambda functions"
 }
+
+variable "state_bucket" {
+    type = string
+    description = "The name of the S3 bucket in which to store the terraform state"
+}
