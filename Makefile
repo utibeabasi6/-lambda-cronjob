1c7d60b0f4e475f2758cf46ffa7074a89f15de9e
utibeabasi6 <utibeabasiumanah6@gmail.com>
2022-02-17 07:05:10 +0100

    update file zip implementation

--- Makefile
+++ Makefile
@@ -1,12 +1,8 @@
-up: zip
+up: 
 	@terraform apply --auto-approve
 
 down: 
 	@terraform destroy --auto-approve
-
-zip:
-	zip -j zips/start_instances.zip functions/start.py 
-	zip -j zips/stop_instances.zip functions/stop.py 
 
 init:
 	@terraform init
--- main.tf
+++ main.tf
@@ -1,17 +1,31 @@
+data "archive_file" "lambda_start_function" {
+  type             = "zip"
+  source_file      = "functions/start.py"
+  output_file_mode = "0666"
+  output_path      = "zips/start_instances.zip"
+}
+
+data "archive_file" "lambda_stop_function" {
+  type             = "zip"
+  source_file      = "functions/stop.py"
+  output_file_mode = "0666"
+  output_path      = "zips/stop_instances.zip"
+}
+
 module "start_lambda" {
   source = "./modules/lambda"
-  lambda_code_hash = filebase64sha256("zips/start_instances.zip")
+  lambda_code_hash = data.archive_file.lambda_start_function.output_base64sha256
   lambda_code_handler = "start.start"
   lambda_function_name = "start_instances"
-  lambda_code_filename = "zips/start_instances.zip"
+  lambda_code_filename = data.archive_file.lambda_start_function.output_path
   schedule_expression = "cron(45 23 ? * 1 *)"
 }
 
 module "stop_lambda" {
   source = "./modules/lambda"
-  lambda_code_hash = filebase64sha256("zips/stop_instances.zip")
+  lambda_code_hash = data.archive_file.lambda_stop_function.output_base64sha256
   lambda_code_handler = "stop.stop"
   lambda_function_name = "stop_instances"
-  lambda_code_filename = "zips/stop_instances.zip"
+  lambda_code_filename = data.archive_file.lambda_stop_function.output_path
   schedule_expression = "cron(15 1 ? * 2 *)"
 }
